3430e5e5fd696fa23b6f943e45a0bf0319f604a0
Аркадий Ежов <prikol93745774@gmail.com>
2026-02-25 16:20:41 +0300

    Добавьте файлы проекта.

---
+++ ValidateName_WPF/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ValidateName_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private const string JSON_PATH = "https://localhost:7166/api/Users";
+        private HttpClient _httpClient;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _httpClient = new HttpClient();
+        }
+
+        private async void GetDatabtn_Click(object sender, RoutedEventArgs e)
+        {
+            var response =await _httpClient.GetStringAsync(JSON_PATH);
+            NameLv.ItemsSource = JsonConvert.DeserializeObject<List<User >> (response);
+        }
+    }
+}
---
+++ ValidateName_WPF/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValidateName_WPF
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public DateOnly BirthDate { get; set; }
+    }
+}
